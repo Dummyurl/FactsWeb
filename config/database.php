e0e92615014190f769ff5679b06b37eec092ed67
Varun2948 <deepak.pradhan048@gmail.com>
2019-04-17 12:50:22 +0545

    Animation of Fade and Bounce Done with All The Html Changes

--- config/database.php
+++ config/database.php
@@ -45,8 +45,8 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'factsnepal'),
-            'username' => env('DB_USERNAME', 'naxalaravel'),
-            'password' => env('DB_PASSWORD', 'khadka'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -54,7 +54,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => array_filter([
+            // 'options' => array_filter([
+            //     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            // ]),
+            'options' => !extension_loaded('pdo_mysql') ? [] : array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]),
         ],
